4aad165ff8eb535a476c551d62ecb913a24a5b92
Mario Blas <marioblasgc@gmail.com>
2015-02-13 00:12:28 +0000

    Add npm.sh

---
+++ npm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Yeoman
+npm install -g yo
+
+# Grunt
+npm install -g grunt-cli
+
+# Gulp
+npm install -g gulp
+
+# Browser Sync
+npm install -g browser-sync
+
+# JSHint
+npm install -g jshint
+
+# CSSLint
+npm install -g csslint
+
+# Trash
+npm install -g trash
