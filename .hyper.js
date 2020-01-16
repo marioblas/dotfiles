3c7c5c2805ea2b3745df6c7bc1b5a7f6ee400f0e
Mario Blas <marioblasgc@gmail.com>
2020-01-16 18:17:47 +0000

    hyper: add hyper-night-owl plugin

--- .hyper.js
+++ .hyper.js
@@ -131,7 +131,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hypercwd", "hyper-night-owl"],
+  plugins: ["hyper-night-owl"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
