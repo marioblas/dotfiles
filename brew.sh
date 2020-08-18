9f0f211afdc98f5597d5b49dee40a4e0c70a3fd2
Mario Blas Gil Cárdenes <marioblasgc@gmail.com>
2020-08-18 19:01:51 +0100

    Homebrew: add imageoptim cask

--- brew.sh
+++ brew.sh
@@ -62,6 +62,7 @@ brew cask install insomnia
 brew cask install mongodb-compass
 brew cask install figma
 brew cask install nucleo
+brew cask install imageoptim
 
 # Remove outdated versions from the cellar
 brew cleanup
